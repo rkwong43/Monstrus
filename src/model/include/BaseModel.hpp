69dfa64c3e986257038ff07a3220a81d092bd4b0
Roger Kwong <rkwong12@gmail.com>
2022-02-03 14:59:25 -0500

    Update game state linked list and add functionality for processing entities one by one

--- src/model/include/BaseModel.hpp
+++ src/model/include/BaseModel.hpp
@@ -8,6 +8,9 @@
 #include "IEntity.hpp"
 #include "LinkedList.hpp"
 
+// TODO: Look for a better way to do this
+#define ENTITY_VECTOR_TYPE std::shared_ptr<std::vector<std::unique_ptr<Entities::IEntity>>>
+
 namespace Game::Model {
     class BaseModel : public IModel {
        public:
@@ -44,7 +47,7 @@ namespace Game::Model {
          * @brief States representing the phases of the state machine the model will go through.
          *
          */
-        enum struct State { SetupAlly, SetupEnemy, ActionAlly, ActionEnemy, ReactionAlly, ReactionEnemy };
+        enum struct State { Setup, Action, Reaction };
 
         /**
          * @brief Generates a starting team of units and sets up the types of units generated in the run.
@@ -60,18 +63,34 @@ namespace Game::Model {
          */
         void updateState();
 
+        /**
+         * @brief Checks the current side to see if empty or index out of range. Cycles to the next side if it is.
+         *
+         */
+        void checkCurrentSide();
+
+        // If the game is over!
+        bool gameOver{false};
+
         // Current state of the model
-        State currentState{State::SetupAlly};
+        State currentState{State::Setup};
 
         // Linked list representing the state machine
-        std::unique_ptr<Utils::ConstLinkedList<State>> states{std::make_unique<Utils::ConstLinkedList<State>>(std::initializer_list<State>(
-            {State::SetupAlly, State::SetupEnemy, State::ActionAlly, State::ActionEnemy, State::ReactionAlly, State::ReactionEnemy}))};
+        std::unique_ptr<Utils::ConstLinkedList<State>> states{
+            std::make_unique<Utils::ConstLinkedList<State>>(std::initializer_list<State>({State::Setup, State::Action, State::Reaction}))};
 
         // Should be limited to 5(?) units for now
         // Replace with a linked list?
         // TODO: Look into some other way of storing them other than nested unique pointers. This is very messy
         // Look into efficiency, but shouldn't be too big of an issue because these aren't meant to be large vectors
-        std::unique_ptr<std::vector<std::unique_ptr<Entities::IEntity>>> allies{std::make_unique<std::vector<std::unique_ptr<Entities::IEntity>>>()};
-        std::unique_ptr<std::vector<std::unique_ptr<Entities::IEntity>>> enemies{std::make_unique<std::vector<std::unique_ptr<Entities::IEntity>>>()};
+        ENTITY_VECTOR_TYPE allies{std::make_shared<std::vector<std::unique_ptr<Entities::IEntity>>>()};
+        ENTITY_VECTOR_TYPE enemies{std::make_shared<std::vector<std::unique_ptr<Entities::IEntity>>>()};
+
+        std::unique_ptr<Utils::ConstLinkedList<ENTITY_VECTOR_TYPE>> entities{
+            std::make_unique<Utils::ConstLinkedList<ENTITY_VECTOR_TYPE>>(std::initializer_list<ENTITY_VECTOR_TYPE>({allies, enemies}))};
+
+        ENTITY_VECTOR_TYPE currentSide{entities->front()};
+        // Index of the current side:
+        int currentIndex{0};
     };
 }  // namespace Game::Model
--- src/model/src/BaseModel.cpp
+++ src/model/src/BaseModel.cpp
@@ -7,52 +7,69 @@
 
 namespace Game::Model {
     void BaseModel::update() {
-        // Actions that are performed each tick
         // TODO: have some sort of lock or blocker so animations can play out etc.
-        // TODO: Add support for enemy phases
-        if (currentState == State::SetupAlly || currentState == State::SetupEnemy) {
-
-        } else if (currentState == State::ActionAlly || currentState == State::ActionEnemy) {
-
-        } else {
-
+        if (gameOver) {
+            std::cout << "Game is over!" << std::endl;
+            return;
+        }
+        switch (currentState) {
+            case State::Setup:
+                setupPhase();
+                break;
+            case State::Action:
+                actionPhase();
+                break;
+            case State::Reaction:
+                reactionPhase();
+                break;
+            default:
+                throw std::runtime_error("Invalid state encountered in model state machine.");
         }
         // More processing?
-        throw std::runtime_error("Invalid state encountered in model state machine.");
+    }
+
+    void BaseModel::checkCurrentSide() {
+        if (currentIndex == currentSide->size() || currentSide->empty()) {
+            // Cycles to the next group
+            currentIndex = 0;
+            entities->next();
+            currentSide = entities->front();
+            // If the new side is empty as well, game is over
+            if (currentSide->empty()) {
+                gameOver = true;
+            }
+        }
     }
 
     void BaseModel::setupPhase() {
-        // TODO: Break into individual steps later on for animations
-        for (auto& ally : *allies) {
-            ally->setUp();
-        }
-        for (auto& enemy : *enemies) {
-            enemy->setUp();
+        // Sets up the current entity:
+        checkCurrentSide();
+        if (!gameOver) {
+            currentSide->at(currentIndex)->setUp();
+            currentIndex++;
         }
     }
 
     void BaseModel::actionPhase() {
-        // Only uses first elements of each
-        // Both allies and enemies should be non-empty
-        if (allies->empty() || enemies->empty()) {
-            throw std::runtime_error("Cannot execute action phase if one of the two parties are empty.");
-        }
+        checkCurrentSide();
         // Handle all the other logic like taking damage, healing, buffs/debuffs etc.
         // Record what happened in a game state of some kind
-        allies->front()->takeAction();
-        enemies->front()->takeAction();
+        if (!gameOver) {
+            currentSide->at(currentIndex)->takeAction();
+            currentIndex++;
+            // Have to find a way to check if the opposing side's front unit or any unit is dead
+        }
         // Checks if either are alive
         // TODO: implement health
     }
 
     void BaseModel::reactionPhase() {
-        // TODO: Break into individual steps later on for animations
         // Find some way to pass down information about what happened (some kind of update or event? A gamestate?)
-        for (auto& ally : *allies) {
-            ally->react();
-        }
-        for (auto& enemy : *enemies) {
-            enemy->react();
+        checkCurrentSide();
+        if (!gameOver) {
+            // Check if unit is dead?
+            currentSide->at(currentIndex)->react();
+            currentIndex++;
         }
     }
 
