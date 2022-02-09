6e888491d934fc19eac00ed86f58bfed0932fbee
rkwong43 <rkwong12@gmail.com>
2022-02-08 19:19:23 -0500

    Add naive implementation of an Action

--- utils/include/Action.hpp
+++ utils/include/Action.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <vector>
 
-namespace Utils::Actions {
+namespace Game::Utils {
+    // What type of action is this?
     enum class ActionType {
         // Decreases HP of specified targets
         ATTACK, 
@@ -12,19 +14,26 @@ namespace Utils::Actions {
         // Decreases Atk of specified targets
         DEBUFF
     };
-    class Action {
-        public:
-            Action() {
-                // TODO: Figure out what parameters need to be added
-            }
 
-        private:
+    // Which team? Or everyone?
+    enum class Side {
+        ALLIES,
+        ENEMIES,
+        ALL
+    };
+
+    struct Action {
+            Action(ActionType type, int value, Side side, std::initializer_list targets) : type(type), value(value){
+                targets = vector<int>(targets);
+            }
             // What type of action?
             ActionType type;
-            // To whom?
-            
-            // What values (damage, healing, etc)?
-            
+            // Value of the action done to the target(s)
+            int value;
+            // What target?
+            Side side;
+            // Target positions (if not all)
+            vector<int> targets;
 
     };
 }
